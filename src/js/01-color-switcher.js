ef2cdbb20e4e37dfb9ace8b3c397498cd13a4dca
Andrii-13 <128071322+Andrii-13@users.noreply.github.com>
2023-08-16 00:34:40 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+let bgc;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+body.addEventListener('click', handlerOnBtn);
+
+function handlerOnBtn(e) {
+  const currentDisabledBtn = document.querySelector('[disabled]');
+
+  if (currentDisabledBtn) {
+    currentDisabledBtn.removeAttribute('disabled');
+  }
+  e.target.setAttribute('disabled', 'disabled');
+  if (e.target.hasAttribute('data-start')) {
+    bgc = setInterval(() => {
+      body.style.backgroundColor = `${getRandomHexColor()}`;
+    }, 1000);
+  }
+  if (e.target.hasAttribute('data-stop')) {
+    clearInterval(bgc);
+  }
+}
